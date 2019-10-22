759f659fa1137ba12b2388705e7ebb59637b3175
Atul Sowani <atul_sowani@persistent.com>
2019-10-22 15:26:29 +0530

    rollback.

--- rollback.c
+++ rollback.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <strings.h>  /* bzero */
 
 int main (void)
 {
   const char *resultFile = "/home/test/tmp/fw_sandbox/fwsim/fwbackup/result";
+  const char *rollbackScript = "/home/test/tmp/fwd/scripts/rollback_app_update.sh";
   FILE *fp;
   int status, tval;
-  char dirStr[15];
+  char dirStr[18];
+  char rollbackCmd [180];
 
+  bzero (&rollbackCmd[0], 180);
+  bzero (&dirStr[0], 18);
   fp = fopen (resultFile, "r");
   fscanf (fp, "%d:%d", &status, &tval);
   fscanf (fp, "%s", &dirStr[0]);
+  fclose (fp);
 
   printf ("status = %d, timestamp = %d, dir = %s\n", status, tval, dirStr);
+  printf ("%s\n", rollbackScript);
+  printf ("%s\n", dirStr);
+
+  sprintf (rollbackCmd, "%s %s", rollbackScript, dirStr);
+  printf ("%s\n", rollbackCmd);
 
   return (0);
 }
